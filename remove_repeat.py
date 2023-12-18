1da92ad77a9db493b9f6b7642da709e34bba30da
liangyouze <youzel@126.com>
2023-12-18 15:47:24 +0800

    update py

--- remove_repeat.py
+++ remove_repeat.py
@@ -1,3 +1,11 @@
+import re
+
+
+def has_two_or_more_numbers(line):
+    numbers = re.findall(r'\d', line)
+    return len(numbers) >= 3
+
+
 def remove_repeat():
     with open('list.txt', 'r+') as f:
         lines = f.readlines()
@@ -5,6 +13,8 @@ def remove_repeat():
         new_lines = []
         for line in lines:
             if line in exist_lines:
+                continue
+            if has_two_or_more_numbers(line):
                 continue
             exist_lines[line] = 1
             new_lines.append(line)
